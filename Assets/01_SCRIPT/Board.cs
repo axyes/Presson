d409410b9f7170befbe2ad8067527fdbb9c60374
Keramien <damien.kermel@gmail.com>
2020-10-09 17:04:02 +0200

    Commentaries in Board.cs

--- Assets/01_SCRIPT/Board.cs
+++ Assets/01_SCRIPT/Board.cs
@@ -2,6 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// Game Board which contain a matrix of slots
+/// </summary>
 public class Board : MonoBehaviour
 {
     // Start is called before the first frame update
