a92c011fa836bf3833d9323eecf77293ea33015f
unknown <bonnec.etienne@gmail.com>
2020-10-12 15:29:18 +0200

    Changement des scène (avec loading), renommage des scènes, création d'UI Container

---
+++ Assets/01_SCRIPT/SceneManagement/Loader.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+/// <summary>
+/// Charge les scènes
+/// </summary>
+public static class Loader
+{
+    /// <summary>
+    /// Enum prenant en compte toutes les scènes
+    /// </summary>
+    public enum Scene
+    {
+        Loading,
+        SplashScreen,
+        Menu,
+        Game,
+        Editor
+    }
+
+    //Delegate
+    private static Action onLoaderCallback;
+
+    /// <summary>
+    /// Charge la scene en passant par le loading
+    /// </summary>
+    /// <param name="scene"></param>
+    public static void LoadScene(Scene scene)
+    {
+        //Chaque fois que onLoadCallback est appelé, charge la scène sélectionnée
+        onLoaderCallback = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+
+        //Charge la scène de loading
+        SceneManager.LoadScene(Scene.Loading.ToString());
+        
+    }
+
+    /// <summary>
+    /// Appelle la fonction onLoaderCallback en Delegate
+    /// </summary>
+    public static void LoaderCallback()
+    {
+        if(onLoaderCallback != null)
+        {
+            onLoaderCallback();
+            onLoaderCallback = null;
+        }
+    }
+}
---
+++ Assets/01_SCRIPT/SceneManagement/UITools.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary>
+/// Gère les UI : permet d'appeler les différente scène (fonctions pour boutons)
+/// </summary>
+public class UITools : MonoBehaviour
+{
+    public Image ImgTestRotate;
+    private void Start()
+    {
+        if (ImgTestRotate != null) ImgTestRotate.transform.localEulerAngles = new Vector3(0, 0, 0);
+    }
+    private void Update()
+    {
+        RotateToTest();
+    }
+    public void LoadMenu()
+    {
+        Debug.Log("Load Menu");
+        Loader.LoadScene(Loader.Scene.Menu);
+    }
+    public void LoadGame()
+    {
+        Debug.Log("Load Game");
+        Loader.LoadScene(Loader.Scene.Game);
+    }
+
+    void RotateToTest()
+    {
+        if(ImgTestRotate != null)
+        ImgTestRotate.transform.localEulerAngles += new Vector3(0, 0, 2);
+    }
+
+}
